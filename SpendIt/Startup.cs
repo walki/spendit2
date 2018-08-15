c13d4f188ba2b36fccc2a61c2d75b2fd56e3a48a
Roger Walker <roger.john.walker@gmail.com>
2018-08-14 21:07:49 -0400

    Working on MVC 6

--- SpendIt/Startup.cs
+++ SpendIt/Startup.cs
@@ -15,6 +15,8 @@ namespace SpendIt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,14 @@ namespace SpendIt
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseStaticFiles();
+
+            // Setup the routes, based on URL
+            app.UseMvc(cfg => {
+                cfg.MapRoute("Default", "/{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" });
             });
+            
         }
     }
 }
---
+++ Spendit/wwwroot/js/index.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    var expenseForm = $("#expenseForm");
+    expenseForm.hide();
+
+    var editButton = $("#editButton");
+    editButton.on("click", function () {
+        console.log("Edit the Expense");
+    });
+
+    var expenseInfo = $(".expense-props li");
+    expenseInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var loginToggle = $("#loginToggle");
+    var popupForm = $(".popup-form");
+
+    loginToggle.on("click", function () {
+        popupForm.fadeToggle(200);
+    });
+
+
+
+});
